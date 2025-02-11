5b16ca913bd02b2739c3680d12fb1dbd8b00f97d
JEFFERY TJ <156276526+JefferyTJ@users.noreply.github.com>
2025-02-11 11:14:57 +0530

    Add files via upload

---
+++ pg.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>package</title>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="st.css">
+</head>
+<body>
+
+<section class="header">
+    <a href="home1.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home1.php">home</a>
+        <a href="ab.php">about</a>
+        <a href="pg.php">package</a>
+        <a href="bk.php">book</a>
+    </nav>  
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+
+<div class="heading" style="background:url(https://www.stayvista.com/blog/wp-content/uploads/2023/11/nature-photographer-29ezCWtMtnM-unsplash-1-scaled-1.jpg) no-repeat">
+    <h1>packages</h1>
+</div>
+<!--pack st -->
+
+<section class="packages">
+    <h1 class="heading-title">top destinations </h1>
+    <div class="box-container">
+    <div class="box">
+        <div class="image">
+            <img src="https://blog.dookinternational.com/images/post-media/ulQFe1712299016.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="https://acko-cms.ackoassets.com/places_to_visit_in_egypt_2_46151d1196.png" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="https://www.leisurekart.com/blog/wp-content/uploads/2024/04/Places-to-visit-in-Mumbai.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+    
+        <div class="box">
+            <div class="image">
+                <img src="https://blogs.traveleva.in/wp-content/uploads/2024/01/best-tourist-places-in-india.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+                <a href="bk.php" class="btn">book now</a>
+            </div>
+        </div>
+    
+
+    <div class="box">
+        <div class="image">
+            <img src="https://www.captureatrip.com/_next/image?url=https%3A%2F%2Fcaptureatrip-cms-storage.s3.ap-south-1.amazonaws.com%2FPlaces_to_visit_in_Jamshedpur_7b0fa69af4.jpg&w=3840&q=50" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="https://www.planetware.com/wpimages/2022/02/world-top-rated-tourist-attractions-chichen-itza-mexico.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="https://www.southernliving.com/thmb/S71gH8RYgrutI6H5MG2J6k6Rdm8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(2870x2028:2872x2030)/GettyImages-1468908187-79e55dbe8078404b83d076e0fa5fcccf.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="https://housing.com/news/wp-content/uploads/2022/11/Famous-tourist-places-in-India-state-compressed.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="https://res.cloudinary.com/rainforest-cruises/images/c_fill,g_auto/f_auto,q_auto/v1661186650/top-10-tourist-attractions-in-india-kailasa-temple/top-10-tourist-attractions-in-india-kailasa-temple.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolore ab laudantium rerum, unde deleniti nemo tempore eius placeat aut, accusamus, eum assumenda eveniet obcaecati quidem quos necessitatibus ex magni!</p>
+            <a href="bk.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    
+    </div>
+
+<div class="load-more"><span class="btn">load more</a></span></div>
+    
+</section>
+
+
+<!-- pac end -->
+
+
+
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home1.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="ab.php"><i class="fas fa-angle-right"></i> about</a>
+            <a href="pg.php"><i class="fas fa-angle-right"></i> package</a>
+            <a href="bk.php"><i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+            <a href="#"><i class="fas fa-phone"></i> +111-222-3333 </a>
+            <a href="#"><i class="fas fa-envelope"></i> jhgfdetyj@gmail.com </a>
+            <a href="#"><i class="fas fa-map"></i> coimbatore, india - 641301 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"><i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"><i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"><i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+    </div>
+
+    <div class="credit"> created by <span> Jeffery </span> | all rights reserved! </div>
+</section>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script src="sct.js"></script>
+
+</body>
+</html>
